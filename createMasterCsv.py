f5a5a4edc1dfb2b8f0d50c112148a6d7fc214c98
482F <ryo.ishikawa.2j@stu.hosei.ac.jp>
2020-12-28 17:29:45 +0900

    インデント幅を修正

--- createMasterCsv.py
+++ createMasterCsv.py
@@ -10,21 +10,21 @@ if not (os.path.isdir("output")):
     os.mkdir("output")
 
 for filename in files:
-  with open('data/' + filename +'.json', 'r', encoding='utf-8') as f:
-      d = json.loads(f.read())
+    with open('data/' + filename +'.json', 'r', encoding='utf-8') as f:
+        d = json.loads(f.read())
 
-  df = pd.io.json.json_normalize(d, sep='_')
-  new_id = "id"
-  if filename == "users":
-    new_id = "users_id"
-  elif filename == "channels":
-    new_id = "channels_id"
-  df = df.rename(columns={"id": new_id})
-  if filename == 'users':
-    df['display_name_custom'] = ""
+    df = pd.io.json.json_normalize(d, sep='_')
+    new_id = "id"
+    if filename == "users":
+        new_id = "users_id"
+    elif filename == "channels":
+        new_id = "channels_id"
+    df = df.rename(columns={"id": new_id})
+    if filename == 'users':
+        df['display_name_custom'] = ""
     
-    for index, row in df.iterrows():
-        display_name_custom = row['name'] if row['profile_display_name_normalized'] == "" else row['profile_display_name_normalized']
-        print(display_name_custom)
-        df.at[index, 'display_name_custom'] = display_name_custom
-  df.to_csv('output/' + filename +'.csv', encoding='utf_8_sig')
+        for index, row in df.iterrows():
+            display_name_custom = row['name'] if row['profile_display_name_normalized'] == "" else row['profile_display_name_normalized']
+            print(display_name_custom)
+            df.at[index, 'display_name_custom'] = display_name_custom
+    df.to_csv('output/' + filename +'.csv', encoding='utf_8_sig')
